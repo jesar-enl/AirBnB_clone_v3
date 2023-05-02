bbf6177e8c1e6015291735758ffbf771591cd7df
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-05-02 09:24:11 +0300

    Add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Create the necessary routes"""
+"""
+Create the necessary routes needed for the app
+"""
 
-# Import necessary libraries
+"""Import necessary libraries"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
